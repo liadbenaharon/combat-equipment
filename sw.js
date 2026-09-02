7846360e4a43190852831cdba43e1fd03b589185
liadbenaharon <liadbenaharon@gmail.com>
2026-09-03 00:34:28 +0300

    Refresh PWA for quantity wheel v1.2.5

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE='combat-equipment-v12';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./equipment-icons.js?v=7','./quantity-shortcut.js?v=1'];
+const CACHE='combat-equipment-v13';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./equipment-icons.js?v=7','./quantity-shortcut.js?v=2'];
 function enhanceHtml(html){
  html=html.replace(/<script src="\.\/equipment-icons\.js[^>]*><\/script>/g,'');
  html=html.replace(/<script src="\.\/quantity-shortcut\.js[^>]*><\/script>/g,'');
  html=html.replace(/<span class="app-version-fixed"[^>]*>v[^<]*<\/span>/g,'');
  html=html.replace(/<div class="app-version"[^>]*>v[^<]*<\/div>/g,'');
- html=html.replace('</body>','<script src="./equipment-icons.js?v=7"></script><script src="./quantity-shortcut.js?v=1"></script></body>');
+ html=html.replace('</body>','<script src="./equipment-icons.js?v=7"></script><script src="./quantity-shortcut.js?v=2"></script></body>');
  return html;
 }
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
