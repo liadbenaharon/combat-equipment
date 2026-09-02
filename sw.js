afc14ca4cbf804691acaaadd6d2d08c89788d0ce
liadbenaharon <liadbenaharon@gmail.com>
2026-09-02 23:42:35 +0300

    Refresh app cache for trainee cleanup logic

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='combat-equipment-v10';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./equipment-icons.js?v=6'];
+const CACHE='combat-equipment-v11';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./equipment-icons.js?v=7'];
 function enhanceHtml(html){
  html=html.replace(/<script src="\.\/equipment-icons\.js[^>]*><\/script>/g,'');
  html=html.replace(/<span class="app-version-fixed"[^>]*>v[^<]*<\/span>/g,'');
  html=html.replace(/<div class="app-version"[^>]*>v[^<]*<\/div>/g,'');
- html=html.replace('</body>','<script src="./equipment-icons.js?v=6"></script></body>');
+ html=html.replace('</body>','<script src="./equipment-icons.js?v=7"></script></body>');
  return html;
 }
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
