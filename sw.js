bf7eb9703bcee87b1e0263f0bfef7038c9015d2a
liadbenaharon <liadbenaharon@gmail.com>
2026-09-03 15:37:18 +0300

    Refresh PWA cache after manifest install fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='combat-equipment-v24';
+const CACHE='combat-equipment-v25';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png','./equipment-icons.js?v=7','./quantity-shortcut.js?v=2','./history-collapse.js?v=1','./attendance.js?v=8','./contacts-count.js?v=1'];
 function enhanceHtml(html){
  html=html.replace(/<script src="\.\/equipment-icons\.js[^>]*><\/script>/g,'');
