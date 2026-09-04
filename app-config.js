1346df2fdca3a5ab8f496237587cdf41fe11876f
liadbenaharon <liadbenaharon@gmail.com>
2026-09-04 10:40:49 +0300

    fix: match full and short attendance names

--- app-config.js
+++ app-config.js
@@ -1,8 +1,8 @@
 (function(root){
   'use strict';
   root.COMBAT_APP=Object.freeze({
-    version:'2.3.0',
-    cache:'combat-equipment-v46',
+    version:'2.3.1',
+    cache:'combat-equipment-v47',
     dataFormat:2
   });
 })(typeof self!=='undefined'?self:globalThis);
