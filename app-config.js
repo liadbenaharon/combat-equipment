e2d3abc2250a96fb08990cb4e337bb9ac53a0633
liadbenaharon <liadbenaharon@gmail.com>
2026-09-04 11:02:53 +0300

    feat: add WhatsApp follow-up for ambiguous names

--- app-config.js
+++ app-config.js
@@ -1,8 +1,8 @@
 (function(root){
   'use strict';
   root.COMBAT_APP=Object.freeze({
-    version:'2.3.2',
-    cache:'combat-equipment-v48',
+    version:'2.3.3',
+    cache:'combat-equipment-v49',
     dataFormat:2
   });
 })(typeof self!=='undefined'?self:globalThis);
