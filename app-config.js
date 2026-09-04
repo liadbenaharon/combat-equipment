57d6109ce59d628f63f443b3f9348a12eda51408
liadbenaharon <liadbenaharon@gmail.com>
2026-09-04 03:48:22 +0300

    fix: force delivery of v2.1 app metadata

--- app-config.js
+++ app-config.js
@@ -2,7 +2,7 @@
   'use strict';
   root.COMBAT_APP=Object.freeze({
     version:'2.1.0',
-    cache:'combat-equipment-v42',
+    cache:'combat-equipment-v43',
     dataFormat:2
   });
 })(typeof self!=='undefined'?self:globalThis);
