c65e62f37b1145e5f301aded378f4ef7cac8369e
liadbenaharon <liadbenaharon@gmail.com>
2026-09-04 10:49:29 +0300

    feat: flag ambiguous attendance names

--- app-config.js
+++ app-config.js
@@ -1,8 +1,8 @@
 (function(root){
   'use strict';
   root.COMBAT_APP=Object.freeze({
-    version:'2.3.1',
-    cache:'combat-equipment-v47',
+    version:'2.3.2',
+    cache:'combat-equipment-v48',
     dataFormat:2
   });
 })(typeof self!=='undefined'?self:globalThis);
