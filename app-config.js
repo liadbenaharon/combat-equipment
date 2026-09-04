27871d26c6ef5b8ff588bdf6ab18713320ee0dee
liadbenaharon <liadbenaharon@gmail.com>
2026-09-04 11:57:36 +0300

    fix: resolve saved contacts for full names

--- app-config.js
+++ app-config.js
@@ -1,8 +1,8 @@
 (function(root){
   'use strict';
   root.COMBAT_APP=Object.freeze({
-    version:'2.3.3',
-    cache:'combat-equipment-v49',
+    version:'2.3.4',
+    cache:'combat-equipment-v50',
     dataFormat:2
   });
 })(typeof self!=='undefined'?self:globalThis);
