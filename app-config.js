9e61f6a80dea06755cfb327c6eb5a97d7eaf45a6
liadbenaharon <liadbenaharon@gmail.com>
2026-09-04 10:27:28 +0300

    chore: prepare Google Play TWA release

--- app-config.js
+++ app-config.js
@@ -1,8 +1,9 @@
 (function(root){
   'use strict';
   root.COMBAT_APP=Object.freeze({
-    version:'2.2.0',
-    cache:'combat-equipment-v45',
+    version:'2.3.0',
+    cache:'combat-equipment-v46',
     dataFormat:2
   });
 })(typeof self!=='undefined'?self:globalThis);
+
